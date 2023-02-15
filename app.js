a3516e8a2957f71679b10617518701ce7f5ac8ab
iu-sumon <md.sumonmia.cse.iu@gmail.com>
2023-02-15 16:56:12 +0600

    mt added

--- app.js
+++ app.js
@@ -67,6 +67,8 @@ for(var i=1; i<10 ; i++)
       
     }
 }
+
+
 var chartDom = document.getElementById('analystRating');
 var myChart = echarts.init(chartDom);
 var option;
@@ -77,14 +79,27 @@ option = {
         type: 'slider',
         handleIcon:
         'path://M15,30C15,21.71270751953125,21.71270751953125,15,30,15C38.28729248046875,15,45,21.71270751953125,45,30C45,38.28729248046875,38.28729248046875,45,30,45C21.71270751953125,45,15,38.28729248046875,15,30Z',
-        xAxisIndex: [0],
+        //xAxisIndex: [0],
         filterMode: 'filter',
-        start: 0,
-        end: 10
+        start: new Date('2019-12-31'),
+        end: new Date('2021-12-31')
+        
     }],
   xAxis: {
-    type: 'category',
-    data: ['2019-12-31', '2020-12-31', '2021-12-31', '2022-12-31', '2023-12-31']
+    type: 'time',
+    data: ['2019-12-31', '2020-12-31', '2021-12-31', '2022-12-31', '2023-12-31'],
+    axisLabel: {
+      formatter: function (value, index) {
+          var date = new Date(value);
+          var year = date.getFullYear();
+          var month = date.getMonth() + 1;
+          month = month < 10 ? '0' + month : month;
+          var day = date.getDate();
+          day = day < 10 ? '0' + day : day;
+          return [year, month, day].join('-');
+      }
+  },
+    
   },
   yAxis: {
     type: 'value'
@@ -99,12 +114,6 @@ option = {
 
 option && myChart.setOption(option);
 
-// myChart.on('dataZoom', function(params) {
-//   console.log(params)
-//   var startValue = params.batch[0].startValue; // Get the start value of the x-axis
-//   var endValue = params.batch[0].endValue; // Get the end value of the x-axis
-//   console.log('Start:', startValue, 'End:', endValue);
-// });
 
 
 myChart.on('datazoom', function (evt) {
@@ -112,11 +121,23 @@ myChart.on('datazoom', function (evt) {
   axis.forEach(x=>{
     //console.log(x)
   })
- // console.log(axis)
-  var starttime = axis[0];
-  var endtime = axis[axis.length-1];
-  console.log(starttime,endtime);
+// console.log(axis[0])
+// console.log(axis[axis.length-1])
+  // var starttime = axis[0];
+  // var endtime = axis[axis.length-1];
+  var starttime = '2019-12-31';
+  var endtime = '2021-12-31';
+  // console.log(starttime)
+  // console.log(endtime);
 });
+
+
+// myChart.on('dataZoom', function(params) {
+//   console.log(params)
+//   var startValue = params.batch[0].startValue; // Get the start value of the x-axis
+//   var endValue = params.batch[0].endValue; // Get the end value of the x-axis
+//   console.log('Start:', startValue, 'End:', endValue);
+// });
 
 // myChart.on('dataZoom', function() {
 //   var option = myChart.getOption();
